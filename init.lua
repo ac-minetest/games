ccfb5698d1baba7cdcb57352a6be760a11ed57ee
rnd <rnd@rnd.com>
2015-05-08 12:12:46 +0200

    sokoban, checkers, maze generator, game of life

---
+++ init.lua
@@ -0,0 +1,6 @@
+-- rnd: collection of puzzle games for minetest
+dofile(minetest.get_modpath("games").."/maze.lua")
+dofile(minetest.get_modpath("games").."/sokoban.lua")
+dofile(minetest.get_modpath("games").."/checkers.lua")
+dofile(minetest.get_modpath("games").."/life.lua")
+print("[games] loaded")
